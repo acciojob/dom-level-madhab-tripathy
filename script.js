d84d7b47cd63a1ab16bdf7c8c45be412345d61ce
Madhab Tripathy <66151264+madhab-tripathy@users.noreply.github.com>
2023-05-20 01:25:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+window.onload = ()=>{
+    let level = document.querySelectorAll("li");
+    for(let i = 0; i < level.length; i++){
+        if(level[i].id === "level") return alert(`The level of the element is: ${i}`)
+    }
+}
